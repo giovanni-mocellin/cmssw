f2b8fbfee4a28e5868c41ac7a270ff59f8dd9b11
giovanni-mocellin <gio.moc@gmail.com>
2019-03-11 19:39:14 +0100

    launcher_sim.py ready now

--- Validation/GEMCosmicMuonStand/test/launcher_sim.py
+++ Validation/GEMCosmicMuonStand/test/launcher_sim.py
@@ -38,72 +38,72 @@ if __name__ == '__main__':
 	geometry_files_creator.geomMaker(run_number)
 	time.sleep(1)
 	
-#	# Compiling after the generation of the geometry files
-#	scramCommand = "scramv1 b -j 8"
-#	scramming = subprocess.Popen(scramCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=srcPath)
-#	while scramming.poll() is None:
-#		line = scramming.stdout.readline()
-#		print(line)
-#	print scramming.stdout.read()
-#	scramming.communicate()
-#	time.sleep(1)
-#	
-#	# Running the CMSSW code
-#	runCommand = "cmsRun runGEMCosmicStand_sim.py"
-#	running = subprocess.Popen(runCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
-#	while running.poll() is None:
-#		line = running.stdout.readline()
-#		print(line)
-#	print running.stdout.read()
-#	running.communicate()
-#	time.sleep(1)
-#	
-#	# Creating folder outside the CMMSW release to put the output files and plots
-#	outDirName = "Results_QC8_run_"+run_number
-#	#---# Remove old version if want to recreate
-#	if (os.path.exists(resDirPath+outDirName)):
-#		rmDirCommand = "rm -rf "+outDirName
-#		rmDir = subprocess.Popen(rmDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=resDirPath)
-#		rmDir.communicate()
-#	#---# Create the new empty folder
-#	resDirCommand = "mkdir "+outDirName
-#	resDir = subprocess.Popen(resDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=resDirPath)
-#	resDir.communicate()
-#	time.sleep(1)
-#	
-#	# Create folders for ouput plots per chamber
-#	SuperChType = runConfig.StandConfiguration
-#	effoutDir = os.path.abspath("launcher_sim.py").split('gemcrs')[0] + outDirName
-#	for i in range (0,30):
-#		if (SuperChType[int(i/2)] != '0'):
-#			plotsDirCommand = "mkdir outPlots_Chamber_Pos_" + str(i)
-#			plotsDirChamber = subprocess.Popen(plotsDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=effoutDir)
-#			plotsDirChamber.communicate()
-#	time.sleep(1)
-#	
-#	# Selecting the correct output file, changing the name and moving to the output folder
-#	out_name = 'out_run_'
-#	for i in range(8-len(run_number)):
-#		out_name = out_name + '0'
-#	out_name = out_name + run_number + '.root'
-#	
-#	mvCommand = "mv temp_" + out_name + " " + out_name
-#	moving = subprocess.Popen(mvCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
-#	moving.communicate()
-#	time.sleep(1)
-#	
-#	mvToDirCommand = "mv " + out_name + " " + resDirPath+outDirName + "/" + out_name
-#	movingToDir = subprocess.Popen(mvToDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
-#	movingToDir.communicate()
-#	time.sleep(1)
-#	
-#	# Efficiency computation & output
-#	effCommand = "root -l -q " + runPath + "efficiency_calculation.c(" + run_number + ",\"" + runPath + "\")"
-#	efficiency = subprocess.Popen(effCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=effoutDir)
-#	while efficiency.poll() is None:
-#		line = efficiency.stdout.readline()
-#		print(line)
-#	print efficiency.stdout.read()
-#	efficiency.communicate()
-#	time.sleep(1)
+	# Compiling after the generation of the geometry files
+	scramCommand = "scramv1 b -j 8"
+	scramming = subprocess.Popen(scramCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=srcPath)
+	while scramming.poll() is None:
+		line = scramming.stdout.readline()
+		print(line)
+	print scramming.stdout.read()
+	scramming.communicate()
+	time.sleep(1)
+	
+	# Running the CMSSW code
+	runCommand = "cmsRun runGEMCosmicStand_sim.py"
+	running = subprocess.Popen(runCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
+	while running.poll() is None:
+		line = running.stdout.readline()
+		print(line)
+	print running.stdout.read()
+	running.communicate()
+	time.sleep(1)
+	
+	# Creating folder outside the CMMSW release to put the output files and plots
+	outDirName = "Results_QC8_run_"+run_number
+	#---# Remove old version if want to recreate
+	if (os.path.exists(resDirPath+outDirName)):
+		rmDirCommand = "rm -rf "+outDirName
+		rmDir = subprocess.Popen(rmDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=resDirPath)
+		rmDir.communicate()
+	#---# Create the new empty folder
+	resDirCommand = "mkdir "+outDirName
+	resDir = subprocess.Popen(resDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=resDirPath)
+	resDir.communicate()
+	time.sleep(1)
+	
+	# Create folders for ouput plots per chamber
+	SuperChType = runConfig.StandConfiguration
+	effoutDir = os.path.abspath("launcher_sim.py").split('gemcrs')[0] + outDirName
+	for i in range (0,30):
+		if (SuperChType[int(i/2)] != '0'):
+			plotsDirCommand = "mkdir outPlots_Chamber_Pos_" + str(i)
+			plotsDirChamber = subprocess.Popen(plotsDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=effoutDir)
+			plotsDirChamber.communicate()
+	time.sleep(1)
+	
+	# Selecting the correct output file, changing the name and moving to the output folder
+	out_name = 'out_run_'
+	for i in range(8-len(run_number)):
+		out_name = out_name + '0'
+	out_name = out_name + run_number + '.root'
+	
+	mvCommand = "mv temp_" + out_name + " " + out_name
+	moving = subprocess.Popen(mvCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
+	moving.communicate()
+	time.sleep(1)
+	
+	mvToDirCommand = "mv " + out_name + " " + resDirPath+outDirName + "/" + out_name
+	movingToDir = subprocess.Popen(mvToDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
+	movingToDir.communicate()
+	time.sleep(1)
+	
+	# Efficiency computation & output
+	effCommand = "root -l -q " + runPath + "efficiency_calculation.c(" + run_number + ",\"" + runPath + "\")"
+	efficiency = subprocess.Popen(effCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=effoutDir)
+	while efficiency.poll() is None:
+		line = efficiency.stdout.readline()
+		print(line)
+	print efficiency.stdout.read()
+	efficiency.communicate()
+	time.sleep(1)
     
