af7d814af4dea2bb43c74ec4b240ed12f7ce9490
giovanni-mocellin <gio.moc@gmail.com>
2022-03-28 20:09:54 +0200

    Fixed paths for loading LUTs

--- CalibMuon/CSCCalibration/python/CSCL1TPLookupTableEP_cff.py
+++ CalibMuon/CSCCalibration/python/CSCL1TPLookupTableEP_cff.py
@@ -19,13 +19,13 @@ CSCL1TPLookupTableEP = cms.ESSource(
     ),
     ## convert pad number to 1/8-strip in ME1a
     padToEsME11aFiles = cms.vstring(
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME11a_even.txt",
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME11a_odd.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME1a_even.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME1a_odd.txt",
     ),
     ## convert pad number to 1/8-strip in ME1b
     padToEsME11bFiles = cms.vstring(
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME11b_even.txt",
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME11b_odd.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME1b_even.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_pad_es_ME1b_odd.txt",
     ),
     ## convert pad number to 1/8-strip in ME21
     padToEsME21Files = cms.vstring(
@@ -34,13 +34,13 @@ CSCL1TPLookupTableEP = cms.ESSource(
     ),
     ## convert eta partition to minimum wiregroup in ME11
     rollToMinWgME11Files = cms.vstring(
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_min_wg_ME11_even.txt",
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_min_wg_ME11_odd.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_l1_min_wg_ME11_even.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_l1_min_wg_ME11_odd.txt",
     ),
     ## convert eta partition to maximum wiregroup in ME11
     rollToMaxWgME11Files = cms.vstring(
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_max_wg_ME11_even.txt",
-        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_max_wg_ME11_odd.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_l1_max_wg_ME11_even.txt",
+        "L1Trigger/CSCTriggerPrimitives/data/GEMCSC/CoordinateConversion/GEMCSCLUT_roll_l1_max_wg_ME11_odd.txt",
     ),
      ## convert eta partition to minimum wiregroup in ME21
     rollToMinWgME21Files = cms.vstring(
