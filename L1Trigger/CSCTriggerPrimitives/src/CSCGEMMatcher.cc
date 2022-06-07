330c5c5eb0b577e02acd2c088cf5c0100ecf133a
giovanni-mocellin <gio.moc@gmail.com>
2022-06-07 18:01:57 +0200

    Added brackets in CMSGEMMatcher in if statement

--- L1Trigger/CSCTriggerPrimitives/src/CSCGEMMatcher.cc
+++ L1Trigger/CSCTriggerPrimitives/src/CSCGEMMatcher.cc
@@ -154,7 +154,7 @@ void CSCGEMMatcher::matchingClustersLoc(const CSCCLCTDigi& clct,
     bool isMatchedLayer2 = false;
 
     if (cl.id1().layer() == 1) { // cluster has valid layer 1
-      if ((station_ == 1 and enable_match_gem_me1a_ and (isME1a and cl.roll1() == 8 or !isME1a and cl.roll1() < 8)) or
+      if ((station_ == 1 and enable_match_gem_me1a_ and ((isME1a and cl.roll1() == 8) or (!isME1a and cl.roll1() < 8))) or
           (station_ == 1 and !enable_match_gem_me1a_ and !isME1a) or
           (station_ == 2)) {
         isLayer2 = false;
@@ -163,7 +163,7 @@ void CSCGEMMatcher::matchingClustersLoc(const CSCCLCTDigi& clct,
       }
     }
     if (cl.id2().layer() == 2) { // cluster has valid layer 2
-      if ((station_ == 1 and enable_match_gem_me1a_ and (isME1a and cl.roll2() == 8 or !isME1a and cl.roll2() < 8)) or
+      if ((station_ == 1 and enable_match_gem_me1a_ and ((isME1a and cl.roll2() == 8) or (!isME1a and cl.roll2() < 8))) or
           (station_ == 1 and !enable_match_gem_me1a_ and !isME1a) or
           (station_ == 2)) {
         isLayer2 = true;
